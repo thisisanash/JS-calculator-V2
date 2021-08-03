a54ac4152cacf908e8f3743bceb52a45292c8269
Muhammad Anash <55632429+thisisanash@users.noreply.github.com>
2021-08-03 12:48:13 +0530

    Fix Calculation Bug

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ let btn = document.querySelectorAll('button');
 let fieldMain = document.querySelector(".main");
 
 
+let checkNaN = (str) => {
+    if (str == "0" || str == "1" || str == "2" || str == "3" || str == "4" || str == "5" || str == "6" || str == "7" || str == "8" || str == "9") {
+        return false
+    }
+    else {
+        return true
+    }
+}
+
+
 let trigno = (str, tri) => {
     let trig = str.value.slice(str.value.indexOf(tri), str.value.indexOf(")") + 1);
 
@@ -22,6 +32,17 @@ let trigno = (str, tri) => {
     str.value = str.value.replace(trig, trigvalue);
 }
 
+let sqrt = (str) => {
+    let base, exponent, result;
+    base = (str.value).slice(0, (str.value).indexOf("^"));
+    exponent = (str.value).slice((str.value).indexOf("^") + 1);
+    result = eval("Math.pow(" + base + "," + exponent + ")");
+    return result;
+}
+
+
+
+
 for (item of btn) {
     item.addEventListener('click', (e) => {
         btnValue = e.target.innerText;
@@ -39,10 +60,49 @@ for (item of btn) {
         if (btnValue === "tan") {
             btnValue = "tan(";
         }
+        if (btnValue == "x^") {
+            btnValue = "^";
+        }
+        if (btnValue === "x²") {
+            btnValue = "^2";
+        }
+        if (btnValue === "√x") {
+            fieldMain.value = Math.sqrt(fieldMain.value);
+        }
+        if (btnValue == "log") {
+            fieldMain.value = Math.log10(fieldMain.value);
+        }
+        if (btnValue == "ln") {
+            fieldMain.value = Math.log(fieldMain.value);
+        }
+
+
+
+        if (btnValue === "+-") {
+            btnValue = "";
+            if (fieldMain.value[0] != "-") {
+                fieldMain.value = "-" + fieldMain.value;
+            }
+            else if (fieldMain.value[0] == "-") {
+                fieldMain.value = fieldMain.value.slice(1);
+            }
+        }
+
+        if (btnValue == "π") {
+            if (checkNaN(fieldMain.value.slice(-1))) {
+                btnvalue = "π";
+            }
+            else {
+                btnValue = "*π";
+            }
+        }
+
         if (btnValue === "backspace") {
             btnValue = "";
             fieldMain.value = fieldMain.value.slice(0, -1);
         } // backspace
+
+        
 
         if (fieldMain.value != "result field") {
             if (btnValue == "=") {
@@ -51,11 +111,7 @@ for (item of btn) {
                 fieldMain.value = fieldMain.value.replace("÷", "/");
                 fieldMain.value = fieldMain.value.replace("x", "*");
 
-                fieldMain.value = fieldMain.value.replace("x", "*");
-                fieldMain.value = fieldMain.value.replace("x", "*");
-                fieldMain.value = fieldMain.value.replace("x", "*");
-
-                fieldMain.value = fieldMain.value.replace("π", "*" + Math.PI);
+                fieldMain.value = fieldMain.value.replace("π", Math.PI);
 
 
                 if (fieldMain.value.includes("sin(")) {
@@ -64,14 +120,18 @@ for (item of btn) {
                 if (fieldMain.value.includes("cos(")) {
                     trigno(fieldMain, "cos(");
                 }
-                if (fieldMain.value.includes("sin(")) {
+                if (fieldMain.value.includes("tan(")) {
                     trigno(fieldMain, "tan(");
                 }
+                if (fieldMain.value.includes("^")) {
+                    fieldMain.value = sqrt(fieldMain);
+                }
+
 
                 fieldMain.value = eval(fieldMain.value);
             }
             else {
-                if (btnValue != "%") {
+                if (btnValue != "%" && btnValue != "log" && btnValue != "ln" && btnValue != "√x" ){
                     fieldMain.value += btnValue;
                 }
             }
@@ -86,142 +146,4 @@ for (item of btn) {
         } // All Clear
     })
 }
-
-
-
-
-
-
-
-// make recourstion with this concept 
-// str.slice(str.indexOf("sin(") +4, str.indexOf(")") );
-// use start =  str.indexOf("sin(") + 4 
-// use end = str.indexOf(")");
-
-// once you extract use start = start + str
-// once you extract use end = end + str
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// for (let i = 0; i < 20; i++) {
-//     document.querySelector(`.grid-${i+1}`).addEventListener("click",
-//     () => {
-//             console.log( parseInt(document.querySelector(`.grid-${i+1}`).innerHTML));
-//         }
-//     );
-// }
-
-
-
-// let value = (valueof) => {
-//     return document.getElementsByClassName(valueof).value;
-// }
-
-
-// let field1 = document.querySelector(".one");
-// let field2 = document.querySelector(".two");
-// let fieldMain = document.querySelector(".main");
-
-
-// let field = 1;
-// let operation;
-
-// let num1 = 0, num2 = 0;
-
-
-// for (item of btn) {
-//     item.addEventListener('click', (e) => {
-//         btnValue = e.target.innerText;
-//         console.log(btnValue);
-
-//         if(fieldMain.value != "result field"){
-//             fieldMain.value += btnValue;
-//         }
-
-//         if (fieldMain.value == "result field") {
-//             fieldMain.value = btnValue;
-//         }        
-
-//         if (btnValue === '÷' || btnValue === 'x' || btnValue === '+' || btnValue === '-') {
-//             field = 2;
-//             operation = btnValue;
-//             console.log("artha field 1");
-//         }
-
-//         if (btnValue === '=') {
-//             field = 1;
-//             console.log(" equel field 1");
-
-//             if (operation == "+") {
-//                 fieldMain.value = parseInt(field1.value) + parseInt(field2.value);
-//                 console.log(parseInt(num1) + parseInt(num2));
-//             }
-//             if (operation == "-") {
-//                 fieldMain.value = parseInt(field1.value) - parseInt(field2.value);
-//                 console.log(parseInt(num1) - parseInt(num2));
-//             }
-//             if (operation == "x") {
-//                 fieldMain.value = parseInt(field1.value) * parseInt(field2.value);
-//                 console.log(parseInt(num1) * parseInt(num2));
-//             }
-//             if (operation == "÷") {
-//                 fieldMain.value = parseInt(field1.value) / parseInt(field2.value);
-//                 console.log(parseInt(num1) / parseInt(num2));
-//             }
-//         }
-
-//         if (btnValue == 0 || btnValue == 1 || btnValue == 2 || btnValue == 3 || btnValue == 4 || btnValue == 5 || btnValue == 6 || btnValue == 7 || btnValue == 8 || btnValue == 9) {
-//             if (field == 1) {
-//                 if (field1.value == "field 1") {
-//                     field1.value = btnValue;
-//                     num1 = btnValue;
-//                 }
-//                 else {
-//                     field1.value += btnValue;
-//                     num1 = num1 + btnValue;
-//                 }
-//                 // console.log(" btn 1 field 1");
-//             }
-
-//             if (field == 2) {
-//                 if (field2.value == "field 2") {
-//                     field2.value = btnValue;
-//                     num2 = btnValue;
-//                 }
-//                 else {
-//                     field2.value += btnValue;
-//                     num2 = num2 + btnValue;
-//                 }
-//                 // console.log(" btn 1 field 2");
-//             }
-//         }
-
-//         if (btnValue === "AC") a{
-//             field1.value = "field 1";
-//             field2.value = "field 2";
-//             fieldMain.value = "result field";
-//             num1 = 0;
-//             num2 = 0;
-
-//         }
-
-//         // console.log(btnValue);
-
-//     }
-//     )
-// }
 
